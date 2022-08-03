3b05998e80c2c1bbd8fa69ba1251e73f415b6783
Ian Farias Paixão <ian.paixao@cwi.com.br>
2022-08-03 19:41:26 -0300

    refact script onsubmit

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,15 @@ const radioOptions = [...document.querySelectorAll('[name="rating"]')];
 radioOptions.forEach(option => option.addEventListener('focus', ({target}) => {
     document.querySelector('.rating-option-checked')?.classList.remove('rating-option-checked');
     target.parentNode.classList.add('rating-option-checked');
-    form.setAttribute('value', target.value)
 }))
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     const submitForm = document.querySelector('.submit-form')
-    const value = document.querySelector('#rating-form').getAttribute('value');
+    const ratingValue = document.querySelector('input[type="radio"]:checked');
     const submitted = document.querySelector('.rating-submitted')
-    const feedbackMessage = `You selected ${value} of 5`;
+    const feedbackMessage = `You selected ${ratingValue} of 5`;
 
     submitForm.style.display = 'none';
     submitted.style.display = 'block'
