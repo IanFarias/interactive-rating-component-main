d6924c43bccc2670843ff753c84a95c84be544e6
Ian Farias Paixão <ian.paixao@cwi.com.br>
2022-08-03 20:06:05 -0300

    fix: get checked value

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,14 @@ const radioOptions = [...document.querySelectorAll('[name="rating"]')];
 radioOptions.forEach(option => option.addEventListener('focus', ({target}) => {
     document.querySelector('.rating-option-checked')?.classList.remove('rating-option-checked');
     target.parentNode.classList.add('rating-option-checked');
+    target.checked = 'true'
 }))
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     const submitForm = document.querySelector('.submit-form')
-    const ratingValue = document.querySelector('input[type="radio"]:checked');
+    const ratingValue = document.querySelector('input[type="radio"]:checked')?.value;
     const submitted = document.querySelector('.rating-submitted')
     const feedbackMessage = `You selected ${ratingValue} of 5`;
 
